c15e6dfaf5d395dc68a659060c9bdb56a9ea9e87
Laure Leter <laure.leter@gmail.com>
2020-08-20 08:53:26 +0200

    Events seed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,29 +2,8 @@ class EventsController < ApplicationController
 
 def index
 
-   @events = [
+   @events = Event.order('start_datetime ASC')
 
-    {
-title: "London Retail Expo",
-datetime: "Monday 14 Oct, 2019",
-location: "London Excel Centre"
-},
-
-{
-title: "Enterprise Sales Training Workshop",
-datetime: "Tuesday 15 Oct, 2019",
-location: "Expert Sales Company Headquarters"
-},
-
-{
-  title: "Ruby Hack Night",
-  datetime: "Friday 18 Oct, 2019",
-  location: "Learnetto Headquarters"
-},
-      {
-        title: "Beginners Salsa dance meetup",
-        datetime: "Saturday 14 Oct, 2019"
-} ]
 end
 
 end
