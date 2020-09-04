806fe03276f018567fcf21e6816eee8fa028b16e
Laure Leter <laure.leter@gmail.com>
2020-09-04 12:15:18 +0200

    correction initializer cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,9 @@
-Rails.application.config.middleware.insert_before0,Rack::Corsdo allow do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
     origins 'http://localhost:3000'
     resource '*',
     headers: :any,
     methods: [:get, :post, :patch, :put, :delete]
-  end
+ end
 end
 
