ff137498c71d7164b7f38aac40fb2d9fe68cfdcb
Laure Leter <laure.leter@gmail.com>
2020-09-09 15:20:23 +0200

    changing url for origin in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://events-react-front.herokuapp.com'
     resource '*',
     headers: :any,
     expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
